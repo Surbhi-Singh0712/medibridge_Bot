990e04370f00dfee4deb52092571c0de15244143
Surbhi-Singh0712 <surbhisinghforlife@gmail.com>
2025-04-25 19:19:51 +0530

    Fix: Bind Flask app to PORT env variable for Render

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def query():
 
 # Run the Flask app
 if __name__ == '__main__':
-       port = int(os.environ.get("PORT", 5000))
-       app.run(debug=False, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))  # 10000 is the default Render port
+    app.run(debug=True, host="0.0.0.0", port=port)
